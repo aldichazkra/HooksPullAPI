5d3e3074378cf3229e58a44d85b78c445b5cbdad
aldichazkra <44045999+aldichazkra@users.noreply.github.com>
2019-06-25 02:56:44 +0700

    Add files via upload

---
+++ telkomsel_hooks.php
@@ -0,0 +1,18 @@
+<?php
+include "telkomsel_functions.php";
+echo "Ambil Nomor Secara Otomatis\n";
+$msisdn = GetMsisdn();
+echo "MSISDN \t\t\t\t : ".$msisdn."\n";
+sleep(4);
+Echo "\n\nTrying Get Your Token \n";
+$token = json_decode(AutoToken($msisdn),true);
+$tokens = $token['idToken'];
+echo "Your Token : $tokens\n";
+echo "\nMemulai PATCHING NEW TOKEN untuk Hooks\n";
+$NEWTOKEN = GetPatchToken($msisdn,$tokens);
+echo "Result $NEWTOKEN";
+echo "\nMemulai HOOKS PULL API untuk Hooks\n";
+$Hooks = GenerateHooks($msisdn,$tokens);
+echo "ID HOOKS :\t $Hooks\n";
+echo "Silahkan Login dengan AKUN dan masukkan HOOKS ID pada menu PULL API REQUEST";
+//AutoToken(GetMsisdn())
